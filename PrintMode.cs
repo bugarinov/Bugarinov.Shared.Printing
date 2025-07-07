dcb8743eb11646aa5ddde56d357cd020a5c4e987
Yuliy Bugarin <bugarindev@yahoo.com>
2025-07-07 13:06:23 +0800

    feat: added print mode

---
+++ PrintMode.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Printing;
+using System.Text;
+
+namespace Bugarinov.Shared.Printing
+{
+    public class PrintMode
+    {
+        public string Name { get; set; }
+        public Duplexing Duplexing { get; set; }
+    }
+}
